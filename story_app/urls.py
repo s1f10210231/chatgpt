c07d3fb2926d70dbba765e3fd47e5ff91537d50b
s1f10210231 <s1f102102313@iniad.org>
2023-08-18 15:47:22 +0900

    generate

--- story_app/urls.py
+++ story_app/urls.py
@@ -3,10 +3,11 @@ from story_app import views
 
 
 
-app_name = 'blog'
+app_name = 'story_app'
 
 
 urlpatterns = [
     path('',views.index,name='index'),
+    path('create/',views.create,name='create'),
 
 ]
