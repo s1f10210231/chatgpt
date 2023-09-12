2cebc29bd0a04dc379cc4c17f0aabd82141d57a0
s1f10210231 <s1f102102313@iniad.org>
2023-09-13 02:44:59 +0900

    build

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 # exit on error
 set -o errexit
 
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
-python manage.py collectstatic --no-input
-python manage.py migrate
-python manage.py newsuperuser
+python3 manage.py collectstatic --no-input
+python3 manage.py migrate
+python3 manage.py newsuperuser
