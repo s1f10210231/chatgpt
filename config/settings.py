457289d33600e1d6e3d5f136577c37cdc7052265
s1f10210231 <s1f102102313@iniad.org>
2023-09-13 13:14:09 +0900

    setting3

--- config/settings.py
+++ config/settings.py
@@ -51,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',    # この行を追加
+
 ]
 
 ROOT_URLCONF = 'config.urls'
