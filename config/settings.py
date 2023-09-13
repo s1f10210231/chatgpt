304f1f3e1123820b78d77d6f2838d220db448f90
s1f10210231 <s1f102102313@iniad.org>
2023-09-13 09:22:38 +0900

    static

--- config/settings.py
+++ config/settings.py
@@ -116,8 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'story_app/static'),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'story_app/staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # 以下を追加
