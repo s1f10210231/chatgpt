29962643f2dcb5f18cb473b27f4e6474c898fd1b
s1f10210231 <s1f102102313@iniad.org>
2023-11-22 17:51:00 +0900

    buildしたい

--- build.sh
+++ build.sh
@@ -5,5 +5,7 @@ set -o errexit
 pip3 install -r requirements.txt
 
 python3 manage.py collectstatic --no-input
+python3 manage.py makemigrations accounts
+python3 manage.py makemigrations story_app
 python3 manage.py migrate
 python3 manage.py newsuperuser
